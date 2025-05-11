9c23ac6f637ed0a2f6ce3728e92d3292e1c61d95
dyl69l <doyun.lee.gbg23@amerikanskagymnasiet.se>
2025-05-11 16:54:49 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+def velocity():
+    c = 299792458  # Speed of light
+
+    u_input = input("Enter U (in m/s): ").lower()
+    v_input = input("Enter V (in m/s): ").lower()
+
+    if "c" in u_input:
+        u = float(u_input.replace("c", "")) * c
+    else:
+        u = float(u_input)
+
+    if "c" in v_input:
+        v = float(v_input.replace("c", "")) * c
+    else:
+        v = float(v_input)
+
+
+    u_prime = (u + v) / (1 + (u * v) / c**2)
+
+    if u_prime >= c:
+        print("It is impossible")
+    else:
+        print(f"Resulting velocity: {u_prime:.4f} m/s")
+        print(f"As a fraction of the speed of light: {u_prime / c:.8f} c")
